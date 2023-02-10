b8cf6d6dd90c28dad70e4f72dedcdd427fafaf9b
o.laptev.2020 <o.laptev.2020@CLOUD-RDHOST1>
2023-02-10 12:07:46 +0700

    Добавьте файлы проекта.

---
+++ Labirint/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MazeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MazeFactory  factory = new MazeFactory
+
+        
+        }
+    }
+
